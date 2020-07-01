6af8e3c60701153deff8e9c795b3789d9ae616ef
ambiguoustexture <svuvyrfvync@gmail.com>
2020-07-01 20:48:50 +0800

    Finished Assignment1

---
+++ Assignment1/SoftFramework/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/here/Documents/WorkSpace/GAMES101/Assignment1/SoftFramework/Triangle.cpp" "/Users/here/Documents/WorkSpace/GAMES101/Assignment1/SoftFramework/build/CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
+  "/Users/here/Documents/WorkSpace/GAMES101/Assignment1/SoftFramework/main.cpp" "/Users/here/Documents/WorkSpace/GAMES101/Assignment1/SoftFramework/build/CMakeFiles/Rasterizer.dir/main.cpp.o"
+  "/Users/here/Documents/WorkSpace/GAMES101/Assignment1/SoftFramework/rasterizer.cpp" "/Users/here/Documents/WorkSpace/GAMES101/Assignment1/SoftFramework/build/CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
