5c44483934430b84ca86bd3b6e07cddd8033e5b5
ambiguoustexture <svuvyrfvync@gmail.com>
2020-07-04 22:45:51 +0800

    Added the method getColorBilinear of the Class Texture

--- Assignment3/SoftFramework/Texture.hpp
+++ Assignment3/SoftFramework/Texture.hpp
@@ -30,5 +30,24 @@ public:
         return Eigen::Vector3f(color[0], color[1], color[2]);
     }
 
+    Eigen::Vector3f getColorBilinear(float u, float v)
+    {
+        auto  u_img  = u * width,
+              v_img  = (1 - v) * height;
+        float u_floo = std::floor(u_img),
+              u_ceil = std::min((float)width,  std::ceil(u_img)),
+              v_floo = std::floor(v_img),
+              v_ceil = std::min((float)height, std::ceil(v_img)),
+              s      = (u_img - u_floo) / (u_ceil - u_floo),
+              t      = (v_img - v_ceil) / (v_floo - v_ceil);
+        auto u00     = image_data.at<cv::Vec3b>(v_floo, u_floo),
+             u01     = image_data.at<cv::Vec3b>(v_floo, u_ceil),
+             u10     = image_data.at<cv::Vec3b>(v_ceil, u_floo),
+             u11     = image_data.at<cv::Vec3b>(v_ceil, u_ceil),
+             u0      = u00 + s * (u10 - u00),
+             u1      = u01 + s * (u11 - u01),
+             color   = u0  + t * (u1  - u0);
+        return Eigen::Vector3f(color[0], color[1], color[2]);
+    }
 };
 #endif //RASTERIZER_TEXTURE_H
