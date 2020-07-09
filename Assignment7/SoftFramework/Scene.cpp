e27f660ddbc96f7e0a2a021bd4daa959d4697e38
ambiguoustexture <svuvyrfvync@gmail.com>
2020-07-09 11:31:23 +0800

    Rewrite PathTracing in the function castRay

--- Assignment7/SoftFramework/Scene.cpp
+++ Assignment7/SoftFramework/Scene.cpp
@@ -67,57 +67,60 @@ bool Scene::trace(
 Vector3f Scene::castRay(const Ray &ray, int depth) const
 {
     // DONE: Implement Path Tracing Algorithm here
+    
     Vector3f color(0);
-
+    
+    // sampleLight(inter, pdf_light)
     Intersection inter_ray = intersect(ray);
     if (inter_ray.emit.norm() > 0) 
     {
         color = Vector3f(1);
     }
-    else if (inter_ray.happened)
+    else if (inter_ray.happened) 
     {
-        // Attributes of shading point
-        Vector3f p  = inter_ray.coords,
-                 wo = -ray.direction,
-                 N  = normalize(inter_ray.normal);
-        Material* m = inter_ray.m;
+        Vector3f  p  = inter_ray.coords;
+        Vector3f  wo = -ray.direction;
+        Vector3f  N  = normalize(inter_ray.normal);
+        Material* m  = inter_ray.m;
 
-        // Sample light
-        Intersection inter_light;
-        float pdf_light;
-        sampleLight(inter_light, pdf_light);
-        Vector3f x = inter_light.coords,
-                 ws = normalize(x - p),
-                 NN = normalize(inter_light.normal);
-        
-        // Direct illumination
-        Vector3f L_dir(0);
+        Intersection inter; 
+        float pdf_light; sampleLight(inter, pdf_light);
+    
+        // Get x, ws, NN, emit from inter 
+        Vector3f x    = inter.coords;
+        Vector3f ws   = normalize(x - p);
+        Vector3f NN   = normalize(inter.normal);
+        Vector3f emit = inter.emit;
+    
+        // Shoot a ray from p to x
         Intersection inter_dir = intersect(Ray(p, ws));
-        if ((float)(inter_dir.coords - inter_light.coords).norm() < 0.01)
+    
+        // If the ray is not blocked in the middle
+        Vector3f L_dir(0);
+        if ((float)(inter_dir.coords - inter.coords).norm() < 0.01) 
         {
-            L_dir = inter_light.emit 
-                  * m->eval(ws, wo, N) 
-                  * dotProduct(ws, N) 
-                  * dotProduct(-ws, NN)
-                  / dotProduct(x - p, x - p) 
-                  / pdf_light;
+            L_dir = emit * m->eval(ws, wo, N) * dotProduct(ws, N) / dotProduct(-ws, NN) 
+                  / dotProduct(x - p, x - p) / pdf_light;
         }
-
-        // Indirect illumination
+    
+        // L_indir = 0.0
         Vector3f L_indir(0);
-        if (get_random_float() < RussianRoulette)
+    
+        // Test Russian Roulette with probability RussianRoulette
+        // If ray r hit a non-emitting object
+        if (get_random_float() < RussianRoulette) 
         {
-            Vector3f wi = m->sample(wo, N),
-                     indir_shade_color = castRay(Ray(p, wi), depth);
-            L_indir = indir_shade_color 
-                    * m->eval(wi, wo, N) 
-                    * dotProduct(wi, N) 
-                    / m->pdf(wi, wo, N) 
-                    / RussianRoulette;
+            // wi = sample(wo, N)
+            Vector3f wi = m->sample(wo, N);
+            
+            // Trace a ray r(p, wi)
+            L_indir = castRay(Ray(p, wi), depth) * m->eval(wi, wo, N) 
+                    * dotProduct(wi, N) / m->pdf(wi, wo, N) / RussianRoulette;
         }
         L_indir = L_indir * 1.0 / RussianRoulette;
         color = L_dir + L_indir;
-    }
-    return color; 
+    } 
+    // Return L_dir + L_indir
+    return color;
     // DONE
 }
